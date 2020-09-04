689129bdaeb57dab9d7c4231b10ed2f6aee4db29
Pavan Tummala <35374338+s9chroma@users.noreply.github.com>
2020-09-04 18:59:52 +0200

    Allocating a back buffer

--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -1,54 +1,117 @@
 #include <windows.h>
 
+#define internal static
+#define global_variable static
+#define local_persist static
+
+global_variable bool Running;
+
+global_variable BITMAPINFO BitmapInfo;
+global_variable void *BitmapMemory;
+global_variable HBITMAP BitmapHandle;
+global_variable HDC BitmapDeviceContext;
+
+internal void
+Win32ResizeDIBSection(int X, int Y, int Width, int Height)
+{
+    if(BitmapHandle)
+    {
+        DeleteObject(BitmapHandle);
+    }
+    
+    if(!BitmapDeviceContext)
+    {
+        BitmapDeviceContext = CreateCompatibleDC(0);
+    }
+
+    BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
+    BitmapInfo.bmiHeader.biWidth = Width;
+    BitmapInfo.bmiHeader.biHeight = Height;
+    BitmapInfo.bmiHeader.biPlanes = 1;
+    BitmapInfo.bmiHeader.biBitCount = 32;
+    BitmapInfo.bmiHeader.biCompression = BI_RGB;
+    
+    BitmapHandle = CreateDIBSection(
+            BitmapDeviceContext,
+            &BitmapInfo, 
+            DIB_RGB_COLORS,
+            &BitmapMemory,
+            0, 0
+    );
+}
+
+internal void
+Win32UpdateWindow(HDC DeviceContext, int X, int Y, int Width, int Height)
+{
+    StretchDIBits(
+        DeviceContext,
+        X, Y, Width, Height,
+        X, Y, Width, Height,
+        BitmapMemory,
+        &BitmapInfo,
+        DIB_RGB_COLORS,
+        SRCCOPY
+    );
+}
+
 LRESULT CALLBACK
 WindowProc(HWND hwnd,
-          UINT uMsg,
-          WPARAM wParam,
-          LPARAM lParam)
+            UINT uMsg,
+            WPARAM wParam,
+            LPARAM lParam)
 {
-  LRESULT Result = 0;
-
-  switch(uMsg)
-  {
-    case WM_SIZE:
+    LRESULT Result = 0;
+    
+    switch(uMsg)
     {
-      OutputDebugString("WM_SIZE\n");
-    } break;
+        case WM_SIZE:
+        {
+            RECT ClientRect;
+            GetClientRect(
+                hwnd,
+                &ClientRect 
+            );
+            int X = ClientRect.left;
+            int Y = ClientRect.top;
+            int Width = ClientRect.right - ClientRect.left;
+            int Height = ClientRect.bottom - ClientRect.top;
+            Win32ResizeDIBSection(X, Y, Width, Height);  
+        } break;
 
-    case WM_DESTROY:
-    {
-      OutputDebugString("WM_DESTROY\n");
-    } break;
+        case WM_DESTROY:
+        {
+            Running = false;
+        } break;
 
-    case WM_CLOSE:
-    {
-      OutputDebugString("WM_CLOSE\n");
-    } break;
+        case WM_CLOSE:
+        {
+            DestroyWindow(hwnd);
+        } break;
 
-    case WM_ACTIVATEAPP:
-    {
-      OutputDebugString("WM_ACTIVATEAPP\n");
-    } break;
+        case WM_ACTIVATEAPP:
+        {
+            OutputDebugString("WM_ACTIVATEAPP\n");
+        } break;
 
-    case WM_PAINT:
-    {
-      PAINTSTRUCT Paint;
-      HDC DeviceContext = BeginPaint(hwnd, &Paint);
-      int X = Paint.rcPaint.left;
-      int Y = Paint.rcPaint.top;
-      int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
-      int Width = Paint.rcPaint.right - Paint.rcPaint.left;
-      PatBlt(DeviceContext, X, Y, Width, Height, BLACKNESS);
-      EndPaint(hwnd, &Paint);
-    } break;
+        case WM_PAINT:
+        {
+            PAINTSTRUCT Paint;
+            HDC DeviceContext = BeginPaint(hwnd, &Paint);
+            int X = Paint.rcPaint.left;
+            int Y = Paint.rcPaint.top;
+            int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
+            int Width = Paint.rcPaint.right - Paint.rcPaint.left;
+            Win32UpdateWindow(DeviceContext, X, Y, Width, Height);
+            EndPaint(hwnd, &Paint);
+        } break;
 
-    default:
-    {
-      Result = DefWindowProc(hwnd, uMsg, wParam, lParam);
-    } break;
-  }
+        default:
+        {
+            Result = DefWindowProc(hwnd, uMsg, wParam, lParam);
+        } break;
+    }
 
-  return Result;
+    return Result;
 }
 
 int
@@ -57,51 +120,52 @@ WinMain(HINSTANCE hInstance,
         LPSTR     lpCmdLine,
         int       nShowCmd)
 {
-  WNDCLASSA WindowClass = {0};
-  WindowClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
-  WindowClass.lpfnWndProc = WindowProc;
-  WindowClass.hInstance = hInstance;
-  WindowClass.lpszClassName = "HandmadeHeroWindowClass";
+    WNDCLASSA WindowClass = {0};
+    WindowClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
+    WindowClass.lpfnWndProc = WindowProc;
+    WindowClass.hInstance = hInstance;
+    WindowClass.lpszClassName = "HandmadeHeroWindowClass";
 
-  if(RegisterClassA(&WindowClass))
-  {
-    HWND WindowHandle =
-      CreateWindowExA(
-        0,
-        WindowClass.lpszClassName,
-        "Handmade Hero",
-        WS_OVERLAPPEDWINDOW | WS_VISIBLE,
-        CW_USEDEFAULT, CW_USEDEFAULT,
-        CW_USEDEFAULT, CW_USEDEFAULT,
-        0, 0,
-        hInstance,
-        0);
-    if(WindowHandle)
+    if(RegisterClassA(&WindowClass))
     {
-      for(;;)
-      {
-        MSG Message;
-        BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
-        if(MessageResult > 0)
+        HWND WindowHandle =
+            CreateWindowExA(
+                0,
+            WindowClass.lpszClassName,
+            "Handmade Hero",
+            WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+            CW_USEDEFAULT, CW_USEDEFAULT,
+            CW_USEDEFAULT, CW_USEDEFAULT,
+            0, 0,
+            hInstance,
+            0);
+        if(WindowHandle)
         {
-          TranslateMessage(&Message);
-          DispatchMessage(&Message);
+            Running = true;
+            while(Running)
+            {
+                MSG Message;
+                BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
+                if(MessageResult > 0)
+                {   
+                    TranslateMessage(&Message);
+                    DispatchMessage(&Message);
+                }
+                else
+                {
+                    break;
+                }
+            }
         }
         else
         {
-          break;
+            //TODO(s9chroma): Logging
         }
-      }
     }
     else
     {
-      //TODO(s9chroma): Logging
+        //TODO(s9chroma): Logging
     }
-  }
-  else
-  {
-    //TODO(s9chroma): Logging
-  }
 
-	return 0;
+    return 0;
 }
